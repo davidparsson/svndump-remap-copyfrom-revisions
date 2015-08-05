7da70c33acc26fde5d65d8ed1ddf5813f9e05f3e
David Pärsson <david@parsson.se>
2015-08-05 15:55:01 +0200

    Better argument parsing, drop stdout writing support

--- remap_copyfrom_revisions.py
+++ remap_copyfrom_revisions.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 import re
 import sys
+import argparse
 
-def renumber(dump_filename):
-    with open(dump_filename, 'r+') as dump_file:
-        renumber_file(dump_file)
 
-def renumber_file(dump_file):
+def renumber(input_filename, output_filename):
+    with open(input_filename, 'r') as input_file:
+        with open(output_filename, 'w') as output_file:
+            renumber_file(input_file, output_file)
+
+
+def renumber_file(input_file, output_file):
     revision_number_regex = re.compile("^Revision-number: (\d+)$")
     node_copyfrom_rev_regex = re.compile("^Node-copyfrom-rev: (\d+)$")
     known_revisions = []
 
-    for line in dump_file:
+    for line in input_file:
         revision_match = revision_number_regex.match(line)
         if revision_match:
             known_revisions.append(int(revision_match.group(1)))
@@ -22,17 +26,26 @@ def renumber_file(dump_file):
             if not copyfrom_revision in known_revisions:
                 existing_revision = max(filter(lambda item: item < copyfrom_revision, known_revisions))
                 sys.stderr.write("Remapping: %d -> %d\n" % (copyfrom_revision, existing_revision))
-                sys.stdout.write(line.replace(str(copyfrom_revision), str(existing_revision)))
+                output_file.write(line.replace(str(copyfrom_revision), str(existing_revision)))
                 continue
 
-        sys.stdout.write(line)
+        output_file.write(line)
 
+
+def main():
+    parser = argparse.ArgumentParser(description="Modifies Node-copyfrom-revision to existing revisions in Subversion dumps")
+
+    try:
+        parser.add_argument("--input", "-i", type=str, required=True, metavar='FILE',
+            help='existing svn dump file to process')
+        parser.add_argument("--output", "-o", type=str, required=True, metavar='FILE',
+            help='output file')
+        options = parser.parse_args()
+    except ValueError:
+        parser.print_help()
+        return 1
+
+    return renumber(options.input, options.output)
 
 if __name__ == '__main__':
-    if len(sys.argv) > 2:
-        raise Exception("Wrong number of arguments provided")
-    elif len(sys.argv) == 2:
-        sys.exit(renumber(sys.argv[1]))
-    else:
-        sys.exit(renumber_file(sys.stdin))
-
+    sys.exit(main())
